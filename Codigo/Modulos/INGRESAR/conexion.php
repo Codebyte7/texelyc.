9d9503fca271dd0ff4a87a54031aab2c90417186
Codebyte7 <codebyte77@gmail.com>
2024-11-06 16:29:50 -0500

    Delete Codigo directory

--- Codigo/Modulos/INGRESAR/conexion.php
+++
@@ -1,7 +0,0 @@
-<?php
-$conex = new mysqli("localhost", "root", "login");
-
-if ($conexion->connect_errno) {
-    echo "no hay conexion:(" . $conexion->connect_errno . ")" . $conexion->connect_error;
-}
-?>
